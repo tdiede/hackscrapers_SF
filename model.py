dce2b0b3304727d7bcc07118908222779b655e3f
tdiede <tdiede@gmail.com>
2016-10-24 20:38:41 -0700

    Renamed files to standard names.

---
+++ model.py
@@ -0,0 +1,205 @@
+"""Models and database functions for Buildings project."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# This is the connection to the PostgreSQL database; we're getting this through
+# the Flask-SQLAlchemy helper library. On this, we can find the `session`
+# object, where we do most of our interactions (like committing, etc.)
+
+db = SQLAlchemy()
+
+
+##############################################################################
+# Model definitions
+
+class Building(db.Model):
+    """Buildings in SF."""
+
+    __tablename__ = "buildings"
+
+    def __repr__(self):
+        """Show info about the building."""
+
+        return "<Building rank={} building_name={}>".format(self.rank, self.building_name)
+
+    bldg_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    place_id = db.Column(db.String(64))
+    rank = db.Column(db.Integer)
+    status = db.Column(db.String(64), nullable=True)
+    building_name = db.Column(db.String(128))
+    city_id = db.Column(db.Integer, db.ForeignKey('cities.city_id'), nullable=False)
+    lat = db.Column(db.Float)
+    lng = db.Column(db.Float)
+    height_m = db.Column(db.Float, nullable=True)
+    height_ft = db.Column(db.Float, nullable=True)
+    floors = db.Column(db.Integer, nullable=True)
+    completed_yr = db.Column(db.Integer, nullable=True)
+    material = db.Column(db.String(64), nullable=True)
+    use = db.Column(db.String(64), nullable=True)
+
+    # Define relationship to city.
+    city = db.relationship("City",
+                           backref=db.backref("buildings",
+                                              order_by=bldg_id))
+
+
+class City(db.Model):
+    """Cities in the world with tall buildings."""
+
+    __tablename__ = "cities"
+
+    def __repr__(self):
+        """Show info about the city."""
+
+        return "<City rank=%d city=%s bldg_count=%d>" % (self.rank, self.city, self.bldg_count)
+
+    city_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    rank = db.Column(db.Integer)
+    city = db.Column(db.String(64))
+    country = db.Column(db.String(64))
+    bldg_count = db.Column(db.Integer)
+
+
+class Tenant(db.Model):
+    """Tenants in San Francisco tall buildings."""
+
+    __tablename__ = "tenants"
+
+    def __repr__(self):
+        """Show a list of all tenants found by Google Places API."""
+
+        return "<Tenant tenant_id=%d tenant=%s>" % (self.tenant_id, self.tenant)
+
+    tenant_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    tenant = db.Column(db.String(128))
+    place_id = db.Column(db.String(64))
+    bldg_id = db.Column(db.Integer, db.ForeignKey('buildings.bldg_id'), nullable=False)
+
+    # Define relationship to bldg.
+    bldg = db.relationship("Building",
+                           backref=db.backref("tenants",
+                                              order_by=tenant_id))
+
+
+# User
+class User(db.Model):
+    """Users of web app in database."""
+
+    __tablename__ = "users"
+
+    def __repr__(self):
+        """Show info about user."""
+
+        return "<User user_id=%d username=%s password=%s>" % (self.user_id, self.username, self.password)
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    password = db.Column(db.String(64))
+    card_id = db.Column(db.Integer, db.ForeignKey('cards.card_id'), nullable=True)
+
+
+# Card
+class Card(db.Model):
+    """Cards of bldgs created by users of web app in database."""
+
+    __tablename__ = "cards"
+
+    def __repr__(self):
+        """Show info about cards."""
+
+        return "<Card card_id=%d user_id=%d bldg_id=%d>" % (self.card_id, self.user_id, self.bldg_id)
+
+    card_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer)
+    bldg_id = db.Column(db.Integer, db.ForeignKey('buildings.bldg_id'), nullable=False)
+    card_img = db.Column(db.String(256))
+    comments = db.Column(db.String(256))
+
+    # Define relationship to user.
+    user = db.relationship("User",
+                           backref=db.backref("cards",
+                                              order_by=card_id))
+
+
+###############################################################################
+# Definition example_data() for testing
+
+def example_data():
+    """Sample data for testing."""
+  # In case this is run more than once, empty out existing data from each table.
+    User.query.delete()
+    Card.query.delete()
+    Building.query.delete()
+    Tenant.query.delete()
+    City.query.delete()
+
+    user = User(user_id=1,
+                username='me',
+                password='password',
+                card_id=1)
+
+    card = Card(card_id=1,
+                user_id=1,
+                bldg_id=1)
+
+    bldg = Building(bldg_id=1,
+                    place_id='ChIJWdUJpGOAhYARfBVi2TE8daI',
+                    rank=1,
+                    status='Under Construction',
+                    building_name='Salesforce Tower',
+                    city_id=41,
+                    lat=37.7904907,
+                    lng=-122.397125,
+                    height_m=326.1,
+                    height_ft=1070,
+                    floors=61,
+                    completed_yr=2018,
+                    material='composite',
+                    use='office')
+
+    tenant = Tenant(tenant_id=1,
+                    tenant='50 Fremont Center',
+                    place_id='ChIJWdUJpGOAhYARfBVi2TE8daI',
+                    bldg_id=1)
+
+    city = City(city_id=41,
+                rank=40,
+                city='San Francisco',
+                country='United States',
+                bldg_count=109)
+
+    # db.session.add([user, card, bldg, tenant, city])
+
+    db.session.add(user)
+    db.session.add(card)
+
+    db.session.add(bldg)
+    # db.session.flush()
+    db.session.add(tenant)
+    # db.session.flush()
+    db.session.add(city)
+
+    db.session.commit()
+
+
+##############################################################################
+# Helper functions
+
+def connect_to_db(app, db_uri=None):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PostgreSQL database.
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri or 'postgresql:///buildings'
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print "Connected to postgresql DB."
---
+++ seed.py
@@ -0,0 +1,172 @@
+"""Utility file to seed buildings database from data in skyscraper data."""
+
+from server import app
+
+from model import connect_to_db, db
+from model import (Building, City, Tenant)  # my model file for buildings
+
+from google_places import (get_google_places, extract_geo, extract_names)  # my google places api file
+
+import sys
+
+reload(sys)  # Reload does the trick!
+sys.setdefaultencoding('UTF8')
+
+import os
+
+# Whenever seeding, drop existing database and create a new database.
+os.system("dropdb buildings")
+print "dropdb buildings"
+os.system("createdb buildings")
+print "createdb buildings"
+
+
+def load_buildings():
+    """Load buildings from SFbldgs.csv into database."""
+
+    print "Buildings"
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicates.
+    Building.query.delete()
+    Tenant.query.delete()
+
+    bldg_count = 0
+
+    # Read data file and insert data
+    for row in open("seed_data/SFbldgs.csv"):
+        row = row.rstrip()
+        rank, status, building_name, city_name, height_m, height_ft, floors, completed_yr, material, use = row.split(",")
+
+        bldg_count += 1
+        print "*"*80
+        print city_name
+
+        # Get city_id from building's city_name.
+        city_id = City.query.filter_by(city=city_name).first().city_id
+
+        # Make Google Places API request!
+        places = get_google_places(building_name)
+
+        if places:
+            place_id, lat, lng = extract_geo(places)
+
+        else:
+            place_id = 0
+            lat = 0
+            lng = 0
+
+        if height_m == '':
+            height_m = None
+        if height_ft == '':
+            height_ft = None
+        if completed_yr == '':
+            completed_yr = None
+        if floors == '':
+            floors = None
+
+        bldg = Building(place_id=place_id,
+                        rank=rank,
+                        status=status,
+                        building_name=building_name,
+                        lat=lat,
+                        lng=lng,
+                        city_id=city_id,
+                        height_m=height_m,
+                        height_ft=height_ft,
+                        floors=floors,
+                        completed_yr=completed_yr,
+                        material=material,
+                        use=use)
+
+        print bldg
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(bldg)
+
+        db.session.flush()
+
+        if places:
+
+            # Get bldg_id from newly committed building.
+            bldg_id = Building.query.filter_by(place_id=place_id).first().bldg_id
+
+            # Also seed "tenants" table.
+            load_tenants(places, bldg_count, bldg_id)
+
+    # Once we're done, we should commit our work
+    db.session.commit()
+
+
+def load_tenants(places, bldg_count, bldg_id):
+    """Load tenants from Google Places API request into database."""
+
+    print "Tenants"
+
+    place_id, place_names = extract_names(places)
+
+    for place in place_names:
+
+        tenant = Tenant(tenant=place,
+                        place_id=place_id,
+                        bldg_id=bldg_id)
+
+        print place
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(tenant)
+
+    # Once we're done, we should commit our work
+    db.session.commit()
+
+
+# Manual entry for buildings not found by Google Places API.
+# Actually I entered into Google Maps and they updated the data within 24 hours!
+# def load_manual():
+#     hd_bldg = Building.query.filter_by(building_name="Hunter-Dulin Building").one()
+#     atct_bldg.Building.query.filter_by(building_name="San Francisco International Airport FAA Airport Traffic Control Tower (ATCT)").one()
+#     db.session.add(hd_bldg)
+#     db.session.add(atct_bldg)
+#     db.session.commit()
+
+
+def load_cities():
+    """Load cities from GLBcities into database."""
+
+    print "Cities"
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    City.query.delete()
+
+    # Read data file and insert data
+    for row in open("seed_data/GLBcities.csv"):
+        row = row.rstrip()
+        rank, city, country, bldg_count = row.split(",")
+
+        city = City(rank=rank,
+                    city=city,
+                    country=country,
+                    bldg_count=bldg_count)
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(city)
+
+    # Once we're done, we should commit our work
+    db.session.commit()
+
+
+##############################################################################
+# Helper functions
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    # In case tables haven't been created, create them
+    db.drop_all()
+    db.create_all()
+
+    # Import different types of data
+    load_cities()
+    load_buildings()
